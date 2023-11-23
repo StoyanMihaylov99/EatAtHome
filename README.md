5e7e7ae78ac3cb32a41daf1e6c143e0d431e684a
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-11-23 23:24:15 +0200

    Working on front-end parts

--- README.md
+++
@@ -1,2 +0,0 @@
-# EatAtHome
-A personal project for practicing and learning new skills. This will be a REST project for delivering a food for home. 
--- src/main/java/com/example/eatathome/web/HomeController.java
+++ src/main/java/com/example/eatathome/web/HomeController.java
@@ -5,26 +5,23 @@ import com.example.eatathome.service.Impl.RestaurantServiceImpl;
 
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/restaurants")
+@RequestMapping("/")
 public class HomeController {
-    private final RestaurantServiceImpl restaurantService;
 
-
-    public HomeController(RestaurantServiceImpl restaurantService) {
-        this.restaurantService = restaurantService;
-    }
-
-    @GetMapping
-    public ResponseEntity<List<RestaurantDTO>> getAllRestaurant() {
-        List<RestaurantDTO> allRestaurants = restaurantService.getAllRestaurant();
-
-        return ResponseEntity.ok(allRestaurants);
+    @GetMapping("")
+    public ModelAndView indexPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 }
